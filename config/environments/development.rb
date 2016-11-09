7cb073d17dea7b070292aa8d031bdd073d78602f
bayers92 <brianc.ayers@gmail.com>
2016-11-09 13:41:19 -0500

    action mailer for password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,20 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+    # Added to suport Devise
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "ActionMailer00@gmail.com",
+  password: "ILoveBrian2016",
+  :openssl_verify_mode  => 'none'
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,22 @@ Rails.application.configure do
 #     s3_region: ENV.fetch('AWS_REGION'),
 #   }
 # }
+
+  # Default Mailer Host
+  config.action_mailer.default_url_options = { :host => 'urhisto.xyz' }
+  Rails.application.routes.default_url_options[:host] = 'urhisto.xyz'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"],
+  }
 end
